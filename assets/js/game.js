33ca8d2e2d07a9b3f9ca03d295ec070cb3322576
raposamillar <lisa.raposamillar@gmail.com>
2022-07-13 21:41:22 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,12 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
+
+// fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
@@ -21,7 +27,6 @@ var fight = function(enemyName) {
 
   // if player picks "skip" confirm and then stop the loop
   if (promptFight === "skip" || promptFight === "SKIP") {
-    
     //confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -47,9 +52,9 @@ var fight = function(enemyName) {
 
     // award player money for winning
     playerMoney = playerMoney + 20;
+    
     // leave while() loop since enemy is dead
     break;
-
   } else {
     window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
   }
@@ -72,7 +77,26 @@ var fight = function(enemyName) {
 }; // end of fight function
 
 for(var i = 0; i < enemyNames.length; i++) {
+  // if player is still alive, keep fighting
+  if (playerHealth > 0) {
+    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight
     enemyHealth = 50;
-    fight(enemyNames[i]);
+
+    // use debugger to pause script from running and check what's going on at that moment in the code
+    //debugger;
+
+    // pass the picked EnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter fight(pickedEnemyName);
+    fight(pickedEnemyName);
+  }
+  // if player isn't alive, stop the game
+  else {
+    window.alert('You have lost your robot in battle! Gave Over!');
+    break;
+  }
 }
